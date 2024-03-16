f0d31c2d7a692617d8ba82c8c8ed4676b0b7bbcf
SurendarYama <surendaryama@outlook.com>
2024-03-16 07:47:19 +0530

    socke.io init and  connected

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+import { io } from "socket.io-client";
+
 import "./main.css";
 import { home, dashboard } from "pages";
 
-const app = document.querySelector("#app");
+const socket = io("http://localhost:3000", { transports: ["websocket"] });
+socket.on("connect", () => console.log("IO COnnected..."));
 
+const app = document.querySelector("#app");
 const user = localStorage.getItem("user");
 user ? app.append(dashboard()) : app.append(home());
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,8 +4,8 @@ export default function dashboard() {
   const dashboard = document.createElement("div");
   dashboard.setAttribute("id", "dashboardPage");
   dashboard.classList.add("flex");
-  // const user = localStorage.getItem("user");
-  // console.log(user);
+  const user = localStorage.getItem("user");
+  console.log(user);
   dashboard.append(sidebar());
   return dashboard;
 }
