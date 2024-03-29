622d9c5db192bce41821b34d5b48401a678ad1d1
SurendarYama <surendaryama@outlook.com>
2024-03-29 13:34:42 +0530

    home page css refactored

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,23 +3,11 @@ import { header, register, login } from "components";
 export default function home() {
   const home = document.createElement("div");
   home.setAttribute("id", "homePage");
-  home.classList.add("flex", "flex-col", "w-screen", "min-h-full");
+  home.classList.add("home");
   const homeWrapper = document.createElement("div");
-  homeWrapper.classList.add(
-    "flex",
-    "justify-center",
-    "items-center",
-    "h-screen"
-  );
+  homeWrapper.classList.add("home-wrapper");
   const authWrapper = document.createElement("div");
-  authWrapper.classList.add(
-    "flex",
-    "bg-white",
-    "w-6/12",
-    "h-3/5",
-    "rounded-3xl",
-    "shadow-lg"
-  );
+  authWrapper.classList.add("auth-wrapper");
   authWrapper.append(register(), login());
   homeWrapper.append(authWrapper);
   home.append(header(), homeWrapper);
--- src/shared/components/header/index.js
+++ src/shared/components/header/index.js
@@ -2,7 +2,6 @@ import { logo } from "components";
 
 export default function header() {
   const header = document.createElement("header");
-
   header.classList.add("header");
   const createdWithTextNode = document.createElement("span");
   createdWithTextNode.append(document.createTextNode("CricBook"));
