bd118ba15ec336120a24ff02b19a228e88fa211b
SurendarYama <surendaryama@outlook.com>
2024-05-16 15:29:53 +0530

    import converted into dynamic import

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { io } from "socket.io-client";
+(async function () {
+  const { io } = await import("socket.io-client");
+  import("./main.css");
+  const { home, dashboard } = await import("pages");
 
-import "./main.css";
-import { home, dashboard } from "pages";
+  const socket = io("http://localhost:3000", { transports: ["websocket"] });
+  socket.on("connect", () => console.log("IO COnnected..."));
 
-const socket = io("http://localhost:3000", { transports: ["websocket"] });
-socket.on("connect", () => console.log("IO COnnected..."));
+  const app = document.querySelector("#app");
+  const user = localStorage.getItem("user");
 
-const app = document.querySelector("#app");
-const user = localStorage.getItem("user");
-
-user ? app.append(dashboard()) : app.append(home());
+  user ? app.append(dashboard()) : app.append(home());
+})();
